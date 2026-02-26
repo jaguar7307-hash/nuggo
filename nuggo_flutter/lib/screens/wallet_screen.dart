f1844244167f3f986d1998a528250ad9939ce163
seonsaengnim <jaguar7307@gmail.com>
2026-02-26 09:53:39 +0000

    PM974오후 6:5357: ${l574PMl2026. 2. 26.PM오후 6:53e오후 6:53i53e}

--- nuggo_flutter/lib/screens/wallet_screen.dart
+++ nuggo_flutter/lib/screens/wallet_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
 import '../constants/theme.dart';
-import '../providers/app_provider.dart';
 
 // ─────────────────────────────────────────────
 // Model
